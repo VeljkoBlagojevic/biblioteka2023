8221878d63ae9dc80e54c2c80dcd81d1e998e56f
VeljkoBlagojevic <veljkoblagojevic008@gmail.com>
2023-03-09 09:23:42 +0100

    Dodata logicka kontrola u klasi Biblioteka

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -2,6 +2,7 @@ package biblioteka;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import biblioteka.interfejs.BibliotekaIntefejs;
@@ -12,6 +13,12 @@ public class Biblioteka implements BibliotekaIntefejs {
 
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		if(knjiga == null) {
+			throw new NullPointerException("Knjiga je null");
+		}
+		if(knjige.contains(knjiga)) {
+			throw new IllegalArgumentException("Knjiga je duplikat");
+		}
 		knjige.add(knjiga);
 	}
 
@@ -27,9 +34,14 @@ public class Biblioteka implements BibliotekaIntefejs {
 
 	@Override
 	public List<Knjiga> pronadjiKnjigu(Autor autor, long ISBN, String naslov, String izdavac) {
+		if(autor == null && ISBN <= 0 && naslov == null && izdavac == null) {
+			throw new IllegalArgumentException("Niste uneli nijedan kriterijum pretrage");
+		}
+		
 		return knjige.stream()
 				.filter(knjiga -> knjiga.getAutori().equals(autor))
 				.filter(knjiga -> knjiga.getISBN() == ISBN)
+				.filter(knjiga -> naslov != null)
 				.filter(knjiga -> knjiga.getNaslov().contains(naslov.toLowerCase().trim()))
 				.filter(knjiga -> knjiga.getIzdavac().equals(izdavac))
 				.collect(Collectors.toList());
