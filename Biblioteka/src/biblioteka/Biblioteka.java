41f195704dba5679a184613a66dec0fca763f866
VeljkoBlagojevic <veljkoblagojevic008@gmail.com>
2023-03-09 09:11:19 +0100

    Implementirana interfejs Biblioteka

---
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -0,0 +1,38 @@
+package biblioteka;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import biblioteka.interfejs.BibliotekaIntefejs;
+
+public class Biblioteka implements BibliotekaIntefejs {
+
+	private List<Knjiga> knjige = new ArrayList<Knjiga>();
+
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long ISBN, String naslov, String izdavac) {
+		return knjige.stream()
+				.filter(knjiga -> knjiga.getAutori().equals(autor))
+				.filter(knjiga -> knjiga.getISBN() == ISBN)
+				.filter(knjiga -> knjiga.getNaslov().contains(naslov.toLowerCase().trim()))
+				.filter(knjiga -> knjiga.getIzdavac().equals(izdavac))
+				.collect(Collectors.toList());
+	}
+
+}
